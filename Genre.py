a79081e6abe7783ac70d841aff9cbf77c86c0ef2
spikerheado1234 <ahangupta.96@gmail.com>
2019-02-09 23:59:40 +0800

    Implement listGenre feature

--- Genre.py
+++ Genre.py
@@ -1,26 +1,20 @@
 from Command import Command
-
 from GlobalVariables import *
+import urllib.request
+from bs4 import BeautifulSoup
 
 class Genre(Command):
-    # Accepts a httpaddress and returns the contents of the webpage as a string,
-    # and prints it to the command line.
-    def openWebpage(httpAddress):
-        contents = urllib.urlOpen(httpAddress).read()
-        print(contents)
-        return contents
-
-    # Queries a specific genre and returns contents of that webpages genre as a string,
-    # and prints it to the command line.
-    def queryGenre(genre):
-        contents = urllib.urlOpen(goodReadsUrl + "/genre/" + genre)
-        print(contents)
-        return contents
-
-    def queryMostRead(genre):
-        contents = urllib.urlOpen(goodReadsUrl + "/genre/most_read/" + genre)
-        print(contents)
-        return contents
-
     def execute(self, genre):
-
+        listOfTopBooks = []
+        contents = urllib.request.urlopen(goodReadsUrl + "/genres/most_read/" + genre)
+        page_content = BeautifulSoup(contents.read(), "html.parser")
+        for div in page_content.find_all('div', attrs={'class': 'bigBoxBody'}):
+            for divTwo in div.find_all('div', attrs={'class': 'bigBoxContent'}):
+                for divThree in divTwo.find_all('div', attrs={'class': 'coverRow'}):
+                    for divFour in divThree.find_all('div', attrs={'class': 'leftAlignedImage bookBox'}):
+                        for divFive in divFour.find_all('div', attrs={'class': 'coverWrapper'}):
+                            for divSix in divFive.find_all('a'):
+                                listOfTopBooks.append(divSix.img['alt'])
+        
+        for i in range(0, min(len(listOfTopBooks), 10)):
+            print(listOfTopBooks[i])
--- Main.py
+++ Main.py
@@ -2,17 +2,20 @@ import urllib
 from Command import Command
 from Help import Help
 from GlobalVariables import goodReadsUrl, listOfCommands
-
+from Genre import Genre
 
 def mainControlFlow():
     while True:
         temp = None
-        currCommand = input("What command would you like to execute?\n")
-        if currCommand == 'help':
-            temp = Help(currCommand)
+        currCommand = []
+        for i in input("Please input a command: \n").split():
+            currCommand.append(i)
+        if currCommand[0] == 'help':
+            temp = Help(currCommand[0])
             temp.execute()
-        elif currCommand == 'listGenres':
-
+        elif currCommand[0] == 'listGenre':
+            temp = Genre(currCommand[0])
+            temp.execute(currCommand[1])
         else:
             print("Command not recognized, please type help for more information.")
 
